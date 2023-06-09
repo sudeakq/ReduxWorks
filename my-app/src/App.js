c359c5cab617133c4b845424f11f1122ff9f059d
sudeakq <sudeakq@gmail.com>
2023-06-09 12:09:25 +0300

    added map

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -2,6 +2,11 @@ import Sude from "./components/Sude";
 import Deneme from "./components/Deneme";
 function App() {
   const deneme="props mantigi";
+  const arr= [
+    {name:'array1',id:0},
+    {name:'array2',id:1},
+    {name:'array3',id:2},
+  ]
   const clickFunction = () => {
     console.log("sen butona tıkladın")
   }
@@ -10,6 +15,9 @@ function App() {
       {/* component mantigi */}
      <Sude sudeprop={deneme}/>
      <Deneme/>
+
+     {arr.map((ar, i)=>(<div key={i}>{ar.name}</div>))}
+
      <button onClick={clickFunction}>tıkla</button>
     </div>
   );
