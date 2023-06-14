1523da8112f8324fd8108e41cc38567ca4ad37cd
sudeakq <sudeakq@gmail.com>
2023-06-14 16:55:04 +0300

    useRef

---
+++ my-app/deneme.js
@@ -0,0 +1,2 @@
+const input= document.getElementById('input')
+console.log("input",input)
--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,24 +1,25 @@
 import Sude from "./components/Sude";
 import Deneme from "./components/Deneme";
-import { useState } from "react";
+import { useEffect, useRef, useState } from "react";
 
 function App() {
-  const [status,setStatus]= useState(false)
+  const [status,setStatus]= useState("deneme")
+  const firstRef = useRef(0);
 
- if (status){
-  return(
-    <div>statu durumu true</div>
-  )
- }
- 
- else{
-  return(
-    <div>statu durumu false
-      <button onClick={()=> setStatus(!status)}>tıkla</button>
-    </div>
-    
-  )
- }
+  useEffect(()=>{
+    setStatus("deneme2")
+  },[])
+  console.log("firstRef",firstRef.current.value = 5)
+  const reffunc = () => {
+    // firstRef.current
+  }
+ return(
+  <>
+  <div>{status}</div>
+  <input onClick={reffunc} ref={firstRef} placeholder="ara"/>
+  </>
+ )
+
 }
 
 export default App;
